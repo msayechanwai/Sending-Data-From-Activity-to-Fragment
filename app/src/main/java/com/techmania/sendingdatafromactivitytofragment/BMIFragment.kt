99f51133055cd6b34594a6395c161fcc8461b5b7
Aye Chan Wai <ayechanwai.edu@gmail.com>
2024-01-16 21:48:11 +0630

    Activity to Fragment

---
+++ app/src/main/java/com/techmania/sendingdatafromactivitytofragment/BMIFragment.kt
@@ -0,0 +1,29 @@
+package com.techmania.sendingdatafromactivitytofragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class BMIFragment : Fragment() {
+    lateinit var result : TextView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view: View =inflater.inflate(R.layout.fragment_b_m_i, container, false)
+
+        result= view.findViewById(R.id.textViewResult)
+
+        val weight = arguments?.getInt("weight")!!.toInt()
+        val height = arguments?.getInt("height")!!.toInt()
+
+        val bmi : Double=((weight*10000)/(height*height)).toDouble()
+
+        result.text= "Your bmi is $bmi"
+
+        return view
+    }
+}
